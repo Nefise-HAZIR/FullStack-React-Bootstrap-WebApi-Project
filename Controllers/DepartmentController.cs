bac86773cc2a518c4f6e0b6421b3d44cc17ed589
nefise <2019555034@ogr.cu.edu.tr>
2022-09-01 21:04:42 +0300

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+       public HttpResponseMessage Get()
+         {
+ 
+            DataTable table = new DataTable();
+
+            string query = @"select DepartmentID,DepartmentName from dbo.Departments";
+
+            var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+            var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+            using (var da=new SqlDataAdapter(command)){
+            command.CommandType = CommandType.Text;
+            da.Fill(table); //sorguyla dolduruyoruz(query)
+
+             }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table); //api dan çağırildiğinda bu tablo gidecek durumda
+
+         }
+
+        public string Post(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"insert into dbo.Departments values('" + dep.DepartmentName + @"')";
+
+                var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+                var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+                using (var da = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    da.Fill(table); //sorguyla dolduruyoruz(query)
+
+                }
+                return "added succesfully";
+            }
+            catch (Exception)
+            {
+
+                return "failed...";
+            }
+
+        }
+
+        public string Put(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"update dbo.Departments set DepartmentName='"+dep.DepartmentName+
+                    @"' where DepartmentID="+dep.DepartmentID+@"
+                    ";
+
+                var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+                var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+                using (var da = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    da.Fill(table); //sorguyla dolduruyoruz(query)
+
+                }
+                return "Updated succesfully";
+            }
+            catch (Exception)
+            {
+
+                return "failed...";
+            }
+
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"delete from dbo.Departments where DepartmentID= " + id;
+
+                var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+                var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+                using (var da = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    da.Fill(table); //sorguyla dolduruyoruz(query)
+
+                }
+                return "deleted succesfully";
+            }
+            catch (Exception)
+            {
+
+                return "failed...";
+            }
+
+        }
+    }
+   
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+
+            DataTable table = new DataTable();
+
+            string query = @"select EmployeeID,EmployeeName,Department,MailID,CONVERT(varchar(10),DOJ,120) from dbo.Employees";
+
+            var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+            var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+            using (var da = new SqlDataAdapter(command))
+            {
+                command.CommandType = CommandType.Text;
+                da.Fill(table); //sorguyla dolduruyoruz(query)
+
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table); //api dan çağırildiğinda bu tablo gidecek durumda
+
+        }
+
+        public string Post(Employees emp)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"insert into dbo.Employees (EmployeeName,Department,MailID,DOJ) values(
+                 '" + emp.EmployeeName + @"',
+                 '" + emp.Department + @"',
+                 '" + emp.MailID + @"',
+                 '" + emp.DOJ + @"'
+                )";
+
+                var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+                var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+                using (var da = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    da.Fill(table); //sorguyla dolduruyoruz(query)
+
+                }
+                return "added succesfully";
+            }
+            catch (Exception)
+            {
+
+                return "failed...";
+            }
+
+        }
+
+        public string Put(Employees emp)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"update dbo.Employees set
+                    EmployeeName='" + emp.EmployeeName + @"',
+                    Department='" + emp.Department + @"',
+                    MailID='" + emp.MailID + @"',
+                    DOJ='" + emp.DOJ + @"'
+                      where EmployeeID=" + emp.EmployeeID + @"
+                    ";
+
+                var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+                var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+                using (var da = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    da.Fill(table); //sorguyla dolduruyoruz(query)
+
+                }
+                return "Updated succesfully";
+            }
+            catch (Exception)
+            {
+
+                return "failed...";
+            }
+
+        }
+        public string Delete(int id)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"delete from dbo.Employees where EmployeeID= "+id;
+
+                var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString); //sql e bağlanıyoruz
+                var command = new SqlCommand(query, con); //çalıştırıyoruz command ile sorgu-bağlantı sqlCommand ile veriliyor
+
+                using (var da = new SqlDataAdapter(command))
+                {
+                    command.CommandType = CommandType.Text;
+                    da.Fill(table); //sorguyla dolduruyoruz(query)
+
+                }
+                return "deleted succesfully";
+            }
+            catch (Exception)
+            {
+
+                return "failed...";
+            }
+
+        }
+    }
+}
